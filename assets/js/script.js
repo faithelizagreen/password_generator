041dd0fdf24f409fd46ab956ba4397571041aaf5
faithelizagreen <faithgreen@knights.ucf.edu>
2021-09-23 20:51:41 -0500

    Created arrays for password characters.

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+// Created Array of possible character choices
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
+// Created button to generate password
+const generateBtn = document.querySelector('#generate')
+
+//Created function for passowrd questions
+function questions() {
+    let validLength = false;
+    let passwordLength;
+    do {
+        passwordLength = prompt('Please enter the length of your password');
+
+        if (isNaN(passwordLength) || parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128) {
+            alert('Please enter a password length between 8 and 128 characters');
+        } else {
+            validLength = true
+        }
+    }
+    while (!validLength);
+
+    let validCharacter = false;
+    let questionInput = {};
+    do {
+        let askNumbers = confirm("Would you like to include numbers in your password?");
+        let askLowerCase = confirm("Would you like to include lower case letters in your password?");
+        let askUpperCase = confirm("Would you like to include upper case letters in your password?");
+        let askSpecial = confirm("Would you like to include special characters in your password?");
+        questionInput = {
+            length: passwordLength,
+            usesNumbers: askNumbers,
+            usesLowerCase: askLowerCase,
+            usesUpperCase: askUpperCase,
+            usesSpecial: askSpecial
+        }
+        if ((!askNumbers) && (!askLowerCase) && (!askUpperCase) && (!askSpecial)) {
+            alert("Must choose at least one character type.");
+        } else {
+            validCharacter = true
+        }
+    }
+    while (!validCharacter);
+    return questionInput;
+}
+
+//Created function to generate password
+function generatePassword() {
+    let passwordOptions = questions();
+    let passwordCombos = [];
+    let finalPassword = "";
+
+console.log(passwordOptions)
+    if (passwordOptions.usesNumbers) {
+        passwordCombos = passwordCombos.concat(numbers);
+    }
+    if (passwordOptions.usesLowerCase) {
+        passwordCombos = passwordCombos.concat(lowerCase);
+    }
+    if (passwordOptions.usesUpperCase) {
+        passwordCombos = passwordCombos.concat(upperCase);
+    }
+    if (passwordOptions.usesSpecial) {
+        passwordCombos = passwordCombos.concat(special);
+    }
+    console.log(passwordCombos);
+
+    for (var i = 0; i < passwordOptions.length; i++) {
+        finalPassword += passwordCombos.length
+        // finalPassword += passwordCombos[Math.floor(Math.random() * passwordCombos.length)];
+
+    }
+    console.log(finalPassword);
+
+    return finalPassword;
+}
+
+
+function writePassword() {
+    let password = generatePassword();
+    let passwordText = document.querySelector('#password');
+
+    passwordText.value = password;
+}
+
+// Event listeners
+generateBtn.addEventListener('click', writePassword);
