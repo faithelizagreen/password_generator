480153e0a5854c28f47e7bd48033b7b8b74081ee
faithelizagreen <faithgreen@knights.ucf.edu>
2021-09-24 20:33:00 -0500

    Removed duplicate code from Generate Password function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,34 +72,6 @@ function generatePassword() {
 
     return finalPassword;
 }
-// Created function to generate password
-function generatePassword() {
-    let passwordOptions = questions();
-    let passwordCombos = [];
-    let finalPassword = "";
-
-    console.log(passwordOptions)
-    if (passwordOptions.usesNumbers) {
-        passwordCombos = passwordCombos.concat(numbers);
-    }
-    if (passwordOptions.usesLowerCase) {
-        passwordCombos = passwordCombos.concat(lowerCase);
-    }
-    if (passwordOptions.usesUpperCase) {
-        passwordCombos = passwordCombos.concat(upperCase);
-    }
-    if (passwordOptions.usesSpecial) {
-        passwordCombos = passwordCombos.concat(special);
-    }
-    console.log(passwordCombos);
-
-    for (var i = 0; i < passwordOptions.length; i++) {
-        finalPassword += passwordCombos[Math.floor(Math.random() * passwordCombos.length)];
-    }
-    console.log(finalPassword);
-
-    return finalPassword;
-}
 
 // Created function to write password
 function writePassword() {
